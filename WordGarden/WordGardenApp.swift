993c6f2ee3600b2c8f8fc3233f540efaa1274e7a
Vincent Cina <vcina@me.com>
2025-02-25 15:33:41 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by VINCENT CINA on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
